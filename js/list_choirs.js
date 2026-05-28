ce537cc998227c6ab33a04d1e51e18132c777679
mintathr <keywly21@gmal.com>
2026-05-28 18:56:07 +0700

    20260528 rev3

--- js/list_choirs.js
+++ js/list_choirs.js
@@ -3,31 +3,36 @@ const members = [
         name: "Stefany Valentia",
         role: "Soprano",
         desc: "Membawa kejernihan vokal sejak 2016.",
-        img: "img/sopran/stefany_valentia.JPG"
+        img: "img/sopran/stefany_valentia.JPG",
+        posisi: "object-[center_100%]"
     },
     {
         name: "Gitta Nathania",
         role: "Soprano",
         desc: "Penyanyi dengan suara yang lembut dan penuh emosi.",
-        img: "img/sopran/gitta_nathania.JPG"
+        img: "img/sopran/gitta_nathania.JPG",
+        posisi: "object-[center_20%]"
     },
     {
         name: "Belinda Widodo",
         role: "Soprano",
         desc: "Vokalis utama dalam konser tahunan 2023.",
-        img: "img/sopran/belinda_widodo.JPG"
+        img: "img/sopran/belinda_widodo.JPG",
+        posisi: "object-[center_50%]"
     },
     {
         name: "Michelle Fauziek",
         role: "Soprano",
         desc: "Penyanyi dengan suara yang lembut dan penuh emosi.",
-        img: "img/sopran/michelle_fauziek.JPG"
+        img: "img/sopran/michelle_fauziek.JPG",
+        posisi: "object-[center_30%]"
     },
     {
         name: "Nadine Thendean",
         role: "Alto",
         desc: "Kedalaman suara alto yang hangat.",
-        img: "img/alto/Bernadine_Thendean.JPG"
+        img: "img/alto/Bernadine_Thendean.JPG",
+        posisi: "object-[center_90%]"
     },
     {
         name: "Cheryll Veriena",
@@ -39,31 +44,36 @@ const members = [
         name: "Sthefynna",
         role: "Alto",
         desc: "Kedalaman suara alto yang hangat.",
-        img: "img/alto/Brigitta_Sthefynna.JPG"
+        img: "img/alto/Brigitta_Sthefynna.JPG",
+        posisi: "object-[center_20%]"
     },
     {
         name: "Surya Deo Sindhunata",
         role: "tenor",
         desc: "Vokalis tenor dengan suara yang kuat dan ekspresif.",
-        img: "img/tenor/Surya_Deo_Sindhunata.JPG"
+        img: "img/tenor/Surya_Deo_Sindhunata.JPG",
+        posisi: "object-[center_90%]"
     },
     {
         name: "Mikael Septian",
         role: "tenor",
         desc: "Vokalis tenor dengan suara yang kuat dan ekspresif.",
-        img: "img/tenor/Septian_E._Santoso.JPG"
+        img: "img/tenor/Septian_E._Santoso.JPG",
+        posisi: "object-[center_70%]"
     },
     {
         name: "Mario Viegar",
         role: "tenor",
         desc: "Vokalis tenor dengan suara yang kuat dan ekspresif.",
-        img: "img/tenor/Mario_Viegar.JPG"
+        img: "img/tenor/Mario_Viegar.JPG",
+        posisi: "object-[center_30%]"
     },
     {
         name: "Bayu R. Bonaventura",
         role: "tenor",
         desc: "Vokalis tenor dengan suara yang kuat dan ekspresif.",
-        img: "img/tenor/Bayu_R._Bonaventura.JPG"
+        img: "img/tenor/Bayu_R._Bonaventura.JPG",
+        posisi: "object-[center_90%]"
     },
     {
         name: "Yosua Mario",
@@ -75,7 +85,8 @@ const members = [
         name: "Yudhanto Kurniawan P.",
         role: "bass",
         desc: "Suara bass yang mendalam dan penuh karakter.",
-        img: "img/bass/Yudhanto_Koerniawan.JPG"
+        img: "img/bass/Yudhanto_Koerniawan.JPG",
+        posisi: "object-[center_105%]"
     },
     {
         name: "Nikolas Aryaputra",
@@ -91,55 +102,62 @@ const members = [
     },
     {
         name: "Pricilia Claudia",
-        role: "musician",
+        role: "organist",
         desc: "#",
         img: "img/musician/Pricilia_C._Pattynama.JPG"
     },
     {
         name: "Rex Reinaldo",
-        role: "musician",
+        role: "pianis",
         desc: "#",
-        img: "img/musician/Reinaldo_C._Santoso.JPG"
+        img: "img/musician/Reinaldo_C._Santoso.JPG",
+        posisi: "object-[center_80%]"
     },
     {
         name: "Herman Indrakusuma",
-        role: "musician",
+        role: "pianis",
         desc: "#",
         img: "img/musician/Herman_Indrakusuma.JPG"
     },
     {
         name: "Edwin Djuhadi",
-        role: "principal of string departmen",
+        role: "principal of string departments",
         desc: "#",
-        img: "img/musician/Edwin_Djuhadi.JPG"
+        img: "img/musician/Edwin_Djuhadi.JPG",
+        posisi: "object-[center_40%]"
     },
 
 ];
 
 const container = document.getElementById('gallery-container');
 
-// Fungsi untuk merender ke HTML
 members.forEach((member, index) => {
-    // Gunakan 'eager' untuk 2 foto pertama, sisanya 'lazy'
     const loadingStrategy = index < 4 ? 'eager' : 'lazy';
-    // Mengubah semua spasi menjadi tanda hubung
-    const roleClass = member.role.toLowerCase().replace(/\s+/g, '-');
+    
+    // Ambil role dasar (pianis / biola)
+    const baseRole = member.role.toLowerCase().trim();
+    
+    // Gabungkan dengan class musician
+    let finalClass = baseRole;
+    if (['pianis', 'organist', 'biola', 'principal of string departments'].includes(baseRole)) {
+        finalClass += ' musician';
+    }
 
     container.innerHTML += `
-        <div class="member-card ${roleClass}" data-aos="fade-up">
+        <div class="member-card ${finalClass}" data-aos="fade-up">
             <div class="bg-stone-900/40 border border-stone-800 overflow-hidden rounded-sm transition-all duration-500 hover:border-[#C5A059]/50 group">
                 <div class="aspect-[3/4] overflow-hidden bg-stone-800">
                     <img src="${member.img}" 
                          loading="${loadingStrategy}" 
                          alt="${member.name}" 
-                         class="w-full h-full object-cover transition duration-700 group-hover:scale-110 grayscale group-hover:grayscale-0">
+                         class="w-full h-full object-cover ${member.posisi || 'object-center'} transition duration-700 group-hover:scale-110 grayscale group-hover:grayscale-0">
                 </div>
                 <div class="p-3 md:p-5">
                     <h3 class="text-[#F7E7CE] font-serif text-sm md:text-xl tracking-wide">${member.name}</h3>
                     <p class="text-[#C5A059] text-[8px] md:text-[10px] uppercase tracking-widest mt-0.5 md:mt-1 mb-2">${member.role}</p>
-                    <!--<p class="text-stone-400 text-[10px] md:text-sm font-light leading-relaxed line-clamp-2 md:line-clamp-none">${member.desc}</p>-->
                 </div>
             </div>
         </div>
     `;
 });
+
